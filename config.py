59a7e47708e67553ec9b272a8e7fa4ca3f457ef4
erastus-1 <erastuskariuki15@gmail.com>
2020-12-08 06:09:40 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -10,8 +10,9 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "erastuskariuki15@gmail.com"
-    MAIL_PASSWORD = "e1r2a3s4"
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SENDER_EMAIL = 'erastuskariuki15@gmail.com'
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-export SECRET_KEY = 'erass'
-export MAIL_USERNAME='erastuskariuki15@gmail.com'
-export MAIL_PASSWORD='e1r2a3s4'
+export SECRET_KEY = erass
+export MAIL_USERNAME= erastuskariuki15@gmail.com
+export MAIL_PASSWORD= e1r2a3s4
+export DATABASE_URL = postgresql+psycopg2://erastus:Angular2020@localhost/last1
 
 python3.8 manage.py server
