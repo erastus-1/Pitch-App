e3ba745f395c0dfc506a1d15b9efccc0a06980e7
erastus-1 <erastuskariuki15@gmail.com>
2020-12-08 14:37:19 +0300

    delployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
 
 
 login_manager = LoginManager()
