c528f2a0910bd55f1b2a7c7d83e6e865e596a0ab
erastus-1 <erastuskariuki15@gmail.com>
2020-12-13 13:25:31 +0300

    deployment to heroku

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import StringField,PasswordField,BooleanField,SubmitField,TextAreaField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
